7e6a7158473dbc78525831458d2a2ede962f1c5e
krishpatel20217 <krishpatel20217@gmail.com>
2026-08-10 23:10:58 +0530

    session model in register controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import userModel from "../models/user.model.js";
 import crypto from "crypto";
 import jwt from "jsonwebtoken"
 import config from "../config/config.js";
+import sessionModel from "../models/session.model.js";
 
 
 export async function register(req,res){
@@ -30,18 +31,30 @@ export async function register(req,res){
         password:hashedPassword
     })
 
-    const accessToken = jwt.sign({
-        id:user._id
-    },config.JWT_SECRET,
-    {
-        expiresIn : "15m"
-    })
-
     const refreshToken = jwt.sign({
         id:user._id
     },config.JWT_SECRET,{
         expiresIn:"7d"
     })
+
+    const refreshTokenHash = crypto.createHash("sha256").update(refreshToken).digest("hex");
+
+    const session = await sessionModel.create({
+        user: user._id,
+        refreshTokenHash,
+        ip: req.ip,
+        userAgent: req.headers["user-agent"]
+    })
+
+    const accessToken = jwt.sign({
+        id:user._id,
+        sessionId:session._id
+    },config.JWT_SECRET,
+    {
+        expiresIn : "15m"
+    })
+
+    
 
     res.cookie("refreshToken",refreshToken,{
         httpOnly:true,
