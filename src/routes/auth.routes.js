401ffa6b4a975b3f7ead59ad9b4e4517bd567147
krishpatel20217 <krishpatel20217@gmail.com>
2026-08-11 15:56:14 +0530

    login api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -11,6 +11,11 @@ const authRouter = Router();
 authRouter.post("/register",authController.register)
 
 /**
+ * POST /api/auth/login
+ */
+authRouter.post("/login",authController.login)
+
+/**
  * GET /api/auth/get-me
  */
 authRouter.get("/get-me",authController.getMe)
